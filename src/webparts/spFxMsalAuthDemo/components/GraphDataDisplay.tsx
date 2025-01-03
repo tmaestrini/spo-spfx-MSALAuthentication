5003a3579b2f63f72b903bf3409152f696b21491
Tobias Maestrini <tobias@bee365.ch>
2025-01-03 01:42:20 +0100

    add GraphDataDisplay component and refactor SpFxMsalAuthDemo to utilize authentication context

---
+++ src/webparts/spFxMsalAuthDemo/components/GraphDataDisplay.tsx
@@ -0,0 +1,96 @@
+import * as React from "react"
+import { PrimaryButton} from "@fluentui/react"
+import { Textarea } from "@fluentui/react-components";
+import styles from "./SpFxMsalAuthDemo.module.scss"
+import { escape } from '@microsoft/sp-lodash-subset';
+import { AuthenticationContext } from "../../context/AuthenticationContext";
+import { HttpClient } from '@microsoft/sp-http';
+
+type GraphDataDisplayProps = {
+  applicationID: string;
+  tenantIdentifier: string;
+  redirectUri: string;
+  apiCall: string;
+  httpClient: HttpClient;
+}
+
+export const GraphDataDisplay: React.FC<GraphDataDisplayProps> = (props) => {
+  const { isAuthenticated, userScopes, accessToken, reauthenticate } = React.useContext(AuthenticationContext);
+  const { applicationID, tenantIdentifier, redirectUri, httpClient, apiCall } = props;
+
+  const [graphApiCallData, setGraphApiCallData] = React.useState<string>();
+
+  async function loadUserInfoFromGraph(): Promise<void> {
+    console.log(`calling graph with access token: ${accessToken?.substring(0, 100)}...`);
+
+    const response = await httpClient.get(apiCall, HttpClient.configurations.v1, {
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+      }
+    });
+
+    try {
+      if (response.ok) {
+        const responseData = await response.json();
+        console.log('response data:', responseData);
+        setGraphApiCallData(JSON.stringify(responseData, null, 2));
+      } else {
+        setGraphApiCallData(`Error fetching data: ${response.status}`);
+        console.error('Error fetching data:', response.statusText);
+      }
+    } catch (error) {
+      console.error('HTTP request error:', error);
+    }
+  }
+
+  React.useEffect(() => {
+    console.log('reloading user info from graph');
+    loadUserInfoFromGraph().catch(console.error);
+  }, [accessToken]);
+
+  React.useEffect(() => {
+    console.log('reloading user info from graph');
+    loadUserInfoFromGraph().catch(console.error);
+  }, [props]);
+
+  function akquireNewToken(): void { 
+    reauthenticate();
+  } 
+
+  return <section className={`${styles.spFxMsalAuthDemo}`}>
+    <div className={styles.welcome}>
+      <img alt="" src={require('../assets/welcome-light.png')} className={styles.welcomeImage} />
+      <h2>Authentication Info (webpart properties)</h2>
+      <div>Application ID: <strong>{escape(applicationID)}</strong></div>
+      <div>Redirect URI: <strong>{escape(redirectUri)}</strong></div>
+      <div>Tenant URL: <strong>{escape(tenantIdentifier)}</strong></div>
+    </div>
+
+    <div className={styles.welcome}>
+      <h2>① Authentication Infos</h2>
+      <div>Authenticated: {isAuthenticated ? 'Yes' : 'No'}</div>
+    </div>
+
+    {isAuthenticated && (
+      <div className={styles.welcome}>
+        <h2>② Entra ID (App registration)</h2>
+        <h3>Current User Scopes (From Entra ID)</h3>
+        <div><pre style={{ whiteSpace: 'pre-wrap', wordBreak: 'break-word' }}>{userScopes}</pre></div>
+        {isAuthenticated &&
+          <>
+            <Textarea value={accessToken} size="small" resize="both" />
+            <div><PrimaryButton text='Akquire new token' onClick={() => { akquireNewToken() }} /></div>
+            {/* <div><PrimaryButton text='Logout' onClick={() => { }} /></div> */}
+          </>
+        }
+        {graphApiCallData && (
+          <><h2>③ Graph API</h2>
+          <h3>Return value(s) from API call</h3>
+            <>{apiCall}</>
+            <div><pre style={{ whiteSpace: 'pre-wrap', wordBreak: 'break-word' }}>{graphApiCallData}</pre></div>
+          </>
+        )}
+      </div>
+    )}
+  </section>
+}
--- src/webparts/spFxMsalAuthDemo/components/SpFxMsalAuthDemo.tsx
+++ src/webparts/spFxMsalAuthDemo/components/SpFxMsalAuthDemo.tsx
@@ -1,170 +1,20 @@
 import * as React from 'react';
-import styles from './SpFxMsalAuthDemo.module.scss';
 import type { ISpFxMsalAuthDemoProps } from './ISpFxMsalAuthDemoProps';
-import { escape } from '@microsoft/sp-lodash-subset';
 
-import type { MSALOptions } from "@pnp/msaljsclient";
-import { spfi, SPBrowser } from "@pnp/sp";
-import { MSAL } from "@pnp/msaljsclient";
 import "@pnp/sp/webs";
 import "@pnp/sp/site-users/web";
-import { Configuration, PublicClientApplication } from "@azure/msal-browser";
-import { HttpClient } from '@microsoft/sp-http';
-import { PrimaryButton } from '@fluentui/react';
+import { AuthenticationContextProvider } from '../../context/AuthenticationContext';
+import { GraphDataDisplay } from './GraphDataDisplay';
 
 
 export const SpFxMsalAuthDemo: React.FC<ISpFxMsalAuthDemoProps> = (props) => {
-  const { applicationID, tenantIdentifier, scopes, redirectUri, apiCall } = props;
-  const { httpClient } = props;
 
-  const [msalInstance, setMsalInstance] = React.useState<PublicClientApplication>();
-  const [userMail, setUserMail] = React.useState<string>();
-  const [userScopes, setUserScopes] = React.useState<string>();
-  const [accessToken, setAccessToken] = React.useState<string>();
-  const [graphApiCallData, setGraphApiCallData] = React.useState<string>();
-  const [isLoggedIn, setIsLoggedIn] = React.useState<boolean>(false);
-
-  async function loginForSPOAccessTokenByMSAL(): Promise<void> {
-    const spoOptions: MSALOptions = {
-      configuration: {
-        auth: {
-          clientId: applicationID,
-          authority: `https://login.microsoftonline.com/${tenantIdentifier}`,
-        },
-        cache: {
-          claimsBasedCachingEnabled: true // in order to avoid network call to refresh a token every time claims are requested
-        }
-      },
-      authParams: {
-        forceRefresh: false,
-        scopes: [...scopes.split(',')],
-        // scopes: ["https://tmaestrini.sharepoint.com/.default"],
-      }
-    };
-
-    try {
-      const sp = spfi("https://tmaestrini.sharepoint.com/").using(SPBrowser(), MSAL(spoOptions));
-
-      const user = await sp.web.currentUser();
-      setUserMail(user.Email);
-    } catch (error) {
-      console.error('SPO error:', error);
-    }
-  }
-
-  async function loginForGraphAccessTokenByMSAL(): Promise<void> {
-    const config: Configuration = {
-      auth: {
-        clientId: applicationID,
-        authority: `https://login.microsoftonline.com/${tenantIdentifier}`,
-        // redirectUri: 'https://tmaestrini.sharepoint.com/_layouts/15/workbench.aspx',
-      },
-      cache: {
-        cacheLocation: "localStorage",
-        storeAuthStateInCookie: false,
-      },
-    };
-    setIsLoggedIn(false);
-
-    const msalInstance = new PublicClientApplication(config);
-    await msalInstance.initialize();
-    try {
-      setMsalInstance(msalInstance);
-      const result = await msalInstance.acquireTokenSilent({
-        scopes: scopes ? [...scopes.split(',')] : [],
-        account: msalInstance.getAllAccounts()[0]
-      });
-
-      console.log('Silent token result:', result);
-      setUserScopes(result.scopes.join(', '));
-      setAccessToken(result.accessToken);
-      setIsLoggedIn(true);
-    } catch (error) {
-      console.error("Error acquiring token silently:", error);
-    }
-  }
-
-  async function logout(): Promise<void> {
-    if (msalInstance) {
-      await msalInstance.logoutPopup();
-      setIsLoggedIn(false);
-      window.location.reload();
-    }
-  }
-
-
-  async function getUserInfoFromGraph(): Promise<void> {
-    console.log(`calling graph with access token: ${accessToken}`);
-
-    const response = await httpClient.get(apiCall, HttpClient.configurations.v1, {
-      headers: {
-        'Authorization': `Bearer ${accessToken}`
-      }
-    });
-
-    try {
-      if (response.ok) {
-        const responseData = await response.json();
-        console.log('response data:', responseData);
-        setGraphApiCallData(JSON.stringify(responseData, null, 2));
-      } else {
-        setGraphApiCallData(`Error fetching data: ${response.status}`);
-        console.error('Error fetching data:', response.statusText);
-      }
-    } catch (error) {
-      console.error('HTTP request error:', error);
-    }
-  }
-
-  React.useMemo(() => {
-    console.log('reloading');
-    loginForSPOAccessTokenByMSAL().catch(console.error);
-    loginForGraphAccessTokenByMSAL().catch(console.error);
-  }, [props, scopes]);
-
-  React.useMemo(() => {
-    console.log('reloading user info from graph');
-    getUserInfoFromGraph().catch(console.error);
-  }, [accessToken, isLoggedIn]);
+  const { applicationID, tenantIdentifier, scopes, redirectUri } = props;
 
   return (
-    <section className={`${styles.spFxMsalAuthDemo}`}>
-      <div className={styles.welcome}>
-        <img alt="" src={require('../assets/welcome-light.png')} className={styles.welcomeImage} />
-        <h2>Authentication Info (webpart properties)</h2>
-        <div>Application ID: <strong>{escape(applicationID)}</strong></div>
-        <div>Redirect URI: <strong>{escape(redirectUri)}</strong></div>
-        <div>Tenant URL: <strong>{escape(tenantIdentifier)}</strong></div>
-      </div>
-
-      {userMail && (
-        <div className={styles.welcome}>
-          <h2>① Current User Info From SPO</h2>
-          <div>User Mail: <strong>{userMail}</strong></div>
-        </div>
-      )}
-
-      {userScopes && (
-        <div className={styles.welcome}>
-          <h2>② Microsoft Graph</h2>
-          <h3>Current User Scopes (From Entra ID)</h3>
-          <div><pre style={{ whiteSpace: 'pre-wrap', wordBreak: 'break-word' }}>{userScopes}</pre></div>
-          {isLoggedIn &&
-            <>
-              <div><PrimaryButton text='Akquire new token' onClick={loginForGraphAccessTokenByMSAL} /></div>
-              <div><PrimaryButton text='Logout' onClick={logout} /></div>
-            </>
-          }
-          {graphApiCallData && (
-            <><h3>③ Microsoft Graph: Return value from API call</h3>
-              <>{apiCall}</>
-              <div><pre style={{ whiteSpace: 'pre-wrap', wordBreak: 'break-word' }}>{graphApiCallData}</pre></div>
-            </>
-          )}
-        </div>
-      )}
-
-
-    </section>
+    <AuthenticationContextProvider clientId={applicationID} tenantId={tenantIdentifier} scopes={scopes} redirectUri={redirectUri}>
+      <GraphDataDisplay httpClient={props.httpClient}
+        applicationID={applicationID} tenantIdentifier={tenantIdentifier} redirectUri={redirectUri} apiCall={props.apiCall} />
+    </AuthenticationContextProvider>
   )
 }
